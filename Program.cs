cbdd3bbe757d5707e129a4256c3debf3ac468de2
Matheus de Assis <assis4002@gmail.com>
2020-10-27 14:07:29 -0300

    Project Started

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_01_WorkerContracts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
